ac0c67a5900f9c96731946bfcb1d59e09aa53742
5binn <dhxnejqls13@naver.com>
2024-01-18 23:34:15 +0900

    mbti#5(음악,영화 연결)

---
+++ src/main/java/com/start/st/domain/movie/movie.java
@@ -0,0 +1,23 @@
+package com.start.st.domain.movie;
+
+import com.start.st.domain.mbti.entity.Mbti;
+import com.start.st.global.jpa.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class movie extends BaseEntity {
+    @Column(length = 200)
+    private String name;
+    @ManyToOne
+    private Mbti mbti;
+}
