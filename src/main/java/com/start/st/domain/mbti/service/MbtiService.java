f73473c9226b93d35954916df4b4fbb0d804337a
5binn <dhxnejqls13@naver.com>
2024-01-18 22:51:01 +0900

    mbti#2(MBTI등록)

--- src/main/java/com/start/st/domain/mbti/service/MbtiService.java
+++ src/main/java/com/start/st/domain/mbti/service/MbtiService.java
@@ -1,5 +1,6 @@
 package com.start.st.domain.mbti.service;
 
+import com.start.st.domain.mbti.entity.Mbti;
 import com.start.st.domain.mbti.repository.MbtiRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MbtiService {
     private final MbtiRepository mbtiRepository;
+
+    public void create(String name) {
+        Mbti mbti = Mbti.builder()
+                .name(name)
+                .build();
+        this.mbtiRepository.save(mbti);
+    }
 }
--- src/test/java/com/start/st/ApplicationTests.java
+++ src/test/java/com/start/st/ApplicationTests.java
@@ -1,6 +1,7 @@
 package com.start.st;
 
 import com.start.st.domain.article.service.ArticleService;
+import com.start.st.domain.mbti.service.MbtiService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,12 +9,32 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ApplicationTests {
 
-	@Autowired
-	ArticleService articleService;
+    @Autowired
+    ArticleService articleService;
+    MbtiService mbtiService;
 
-	@Test
-	void contextLoads() {
-		articleService.create("제목입니다","내용입니다");
-	}
+    @Test
+    void contextLoads() {
+        articleService.create("제목입니다", "내용입니다");
+    }
 
+    @Test
+    void registerMbti() {
+        mbtiService.create("ISTJ");
+        mbtiService.create("ISTP");
+        mbtiService.create("ISFJ");
+        mbtiService.create("ISFP");
+        mbtiService.create("INTJ");
+        mbtiService.create("INTP");
+        mbtiService.create("INFJ");
+        mbtiService.create("INFP");
+        mbtiService.create("ESTJ");
+        mbtiService.create("ESTP");
+        mbtiService.create("ESFJ");
+        mbtiService.create("ESFP");
+        mbtiService.create("ENTJ");
+        mbtiService.create("ENTP");
+        mbtiService.create("ENFJ");
+        mbtiService.create("ENFP");
+    }
 }
