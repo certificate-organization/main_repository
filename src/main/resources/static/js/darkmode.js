e6515532f9585f749c0f72dee4476349abb567be
kimjeongbae <gprhf651@naver.com>
2024-01-31 00:48:55 +0900

    메인홈 구성 변경, 다크모드 토글추가

--- src/main/resources/static/js/darkmode.js
+++ src/main/resources/static/js/darkmode.js
@@ -1,23 +1,46 @@
-        document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
 
-            if(document.querySelector('.darkmode')){
-                if(localStorage.getItem("darkmode") == 'on'){
+  const darkModeToggle = document.querySelector('#toggle_dark');
+  const darkModeIcon = document.querySelector('.dark_icon');
+  const lightModeIcon = document.querySelector('.light_icon');
 
-                    document.body.dataset.darkmode='on';
-                    document.querySelector('#toggle-radio-dark').checked = true;
-                }
+  const applyDarkModeStyles = () => {
+    document.body.dataset.dark_mode = 'on';
+    localStorage.setItem("dark_mode", "on");
+  };
 
-                document.querySelector('.darkmode').addEventListener("click", e=>{
-                    if(e.target.classList.contains('todark')){
-                        document.body.dataset.darkmode='on';
-                        localStorage.setItem("darkmode", "on");
-                    }else if(e.target.classList.contains('tolight')){
-                        document.body.dataset.darkmode='off';
-                        localStorage.setItem("darkmode", "off");
-                    }
-                },false);
-            }else{
-                localStorage.removeItem("darkmode");
-            }
+  const applyLightModeStyles = () => {
+    document.body.dataset.dark_mode = 'off';
+    localStorage.setItem("dark_mode", "off");
+  };
 
-        })
+  if (darkModeToggle) {
+    // Check user preference from localStorage
+    const userPrefersDarkMode = localStorage.getItem('dark_mode') === 'on';
+
+    // Apply styles based on user preference
+    if (userPrefersDarkMode) {
+      applyDarkModeStyles();
+      darkModeToggle.checked = true;
+    } else {
+      applyLightModeStyles();
+    }
+
+    // Toggle styles when dark mode icon is clicked
+    darkModeIcon.addEventListener("click", applyDarkModeStyles);
+    
+    // Toggle styles when light mode icon is clicked
+    lightModeIcon.addEventListener("click", applyLightModeStyles);
+
+    // Toggle styles when the toggle input is changed
+    darkModeToggle.addEventListener("change", function () {
+      if (darkModeToggle.checked) {
+        applyDarkModeStyles();
+      } else {
+        applyLightModeStyles();
+      }
+    });
+  } else {
+    localStorage.removeItem("dark_mode");
+  }
+});
