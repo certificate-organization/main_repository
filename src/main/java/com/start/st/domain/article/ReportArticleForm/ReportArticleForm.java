1c521d0c7d250fb6063251680b06496ad51e5ebe
깃ãhanseon97 <hanseon97@naver.com>
2024-02-06 18:11:03 +0900

    신고 완성

--- src/main/java/com/start/st/domain/article/ReportArticleForm/ReportArticleForm.java
+++ src/main/java/com/start/st/domain/article/ReportArticleForm/ReportArticleForm.java
@@ -9,4 +9,16 @@ import lombok.Setter;
 public class ReportArticleForm {
     @NotEmpty(message = "신고 내용은 필수입력 사항입니다.")
     private String reportContent;
+
+    private String badSpeak;
+    private String badAdvertise;
+    private String badContent;
+
+    private String selectedValue;
+
+    private String subject;
+
+    private String radioButtonValue;
+
+    private String reportType;
 }
