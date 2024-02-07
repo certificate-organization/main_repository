16bde149401d2c7ef79dda4c8a64deccfa841288
kimjeongbae <gprhf651@naver.com>
2024-02-07 21:09:52 +0900

    메인 홈페이지 로ê고 및 다크모드

--- src/test/java/com/start/st/ApplicationTests.java
+++ src/test/java/com/start/st/ApplicationTests.java
@@ -48,56 +48,56 @@ class ApplicationTests {
     @Autowired
     MemberService memberService;
 
-    @Test
-    void registerMember() { // 어드민 계정 생성
-        Mbti mbti = mbtiService.getMbti(1L);
-        memberService.create("admin", "1234", "관리자",
-                "admin@email.com", mbti);
-
-    }
-
-    @Test
-    void makeMember() { // 더미 계정 9개 생성  아이디 비번 각각 111, 222 , ... , 999 mbti는 ISTJ로 통일
-        Mbti mbti = mbtiService.getMbti(1L);
-        for (int i = 1; i < 10; i++) {
-            memberService.create("" + i + i + i, "" + i + i + i, "" + i + i + i, "" + i + i + i + "@email.com", mbti);
-        }
-    }
-
-    @Test
-    void makeArticle() { // 더미 계정으로 랜덤한 작성자를 갖는 게시글 199개 생성
-        Mbti mbti = mbtiService.getMbti(1L);
-        Random random = new Random();
-        for (int i = 1; i < 200; i++) {
-            Long randomNumber = (long) (random.nextDouble() * (10 - 2 + 1)) + 2;
-            Member member = memberService.findByMemberId(randomNumber);
-            articleService.create(i + "번 게시글 제목", i + "번 게시글 내용 입니다.", mbti, member);
-        }
-    }
-
-    @Test
-    void makeViewCount() { // 게시글 들에 5000까지 랜덤한 조회수 부여
-        Random random = new Random();
-        for (int i = 1; i < 200; i++) {
-            Long randomNumber = (long) (random.nextDouble() * 5001);
-            Long id = (long) i;
-            Article article = articleService.getArticle(id);
-            articleService.view(article, randomNumber);
-        }
-    }
-
-    @Autowired
-    CommentService commentService;
-
-    @Test
-    void makeComment() { // 게시글 마다 2개의 댓글 생성
-        for (int i = 1; i < 200; i++) {
-            Long id = (long) i;
-            Article article = articleService.getArticle(id);
-            Member member = memberService.getMember("admin");
-            commentService.create(article, "1번 댓글 입니다.", member, null);
-            commentService.create(article, "2번 댓글 입니다.", member, null);
-        }
-    }
+//    @Test
+//    void registerMember() { // 어드민 계정 생성
+//        Mbti mbti = mbtiService.getMbti(1L);
+//        memberService.create("admin", "1234", "관리자",
+//                "admin@email.com", mbti);
+//
+//    }
+//
+//    @Test
+//    void makeMember() { // 더미 계정 9개 생성  아이디 비번 각각 111, 222 , ... , 999 mbti는 ISTJ로 통일
+//        Mbti mbti = mbtiService.getMbti(1L);
+//        for (int i = 1; i < 10; i++) {
+//            memberService.create("" + i + i + i, "" + i + i + i, "" + i + i + i, "" + i + i + i + "@email.com", mbti);
+//        }
+//    }
+//
+//    @Test
+//    void makeArticle() { // 더미 계정으로 랜덤한 작성자를 갖는 게시글 199개 생성
+//        Mbti mbti = mbtiService.getMbti(1L);
+//        Random random = new Random();
+//        for (int i = 1; i < 200; i++) {
+//            Long randomNumber = (long) (random.nextDouble() * (10 - 2 + 1)) + 2;
+//            Member member = memberService.findByMemberId(randomNumber);
+//            articleService.create(i + "번 게시글 제목", i + "번 게시글 내용 입니다.", mbti, member);
+//        }
+//    }
+//
+//    @Test
+//    void makeViewCount() { // 게시글 들에 5000까지 랜덤한 조회수 부여
+//        Random random = new Random();
+//        for (int i = 1; i < 200; i++) {
+//            Long randomNumber = (long) (random.nextDouble() * 5001);
+//            Long id = (long) i;
+//            Article article = articleService.getArticle(id);
+//            articleService.view(article, randomNumber);
+//        }
+//    }
+//
+//    @Autowired
+//    CommentService commentService;
+//
+//    @Test
+//    void makeComment() { // 게시글 마다 2개의 댓글 생성
+//        for (int i = 1; i < 200; i++) {
+//            Long id = (long) i;
+//            Article article = articleService.getArticle(id);
+//            Member member = memberService.getMember("admin");
+//            commentService.create(article, "1번 댓글 입니다.", member, null);
+//            commentService.create(article, "2번 댓글 입니다.", member, null);
+//        }
+//    }
 
 }
